9167bb813e95835ea5834aed0f1e4673d1b24d75
oopserv <oopserv@gmail.com>
2017-05-22 15:57:53 -0500

    add form mutation

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+
+import App from './components/App'
+import SongCreate from './components/SongCreate'
 
 import SongList from './components/SongList'
 
@@ -10,7 +14,12 @@ const client = new ApolloClient({})
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path='/' component={App}>
+          <IndexRoute component={SongList} />
+          <Route path='song/new' component={SongCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 }
