b4b6c14d70e36dacb2970d6b52d41f56c5d50d6f
oopserv <oopserv@gmail.com>
2017-05-22 16:55:16 -0500

    refactor for refetching queries

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link, hashHistory } from 'react-router'
+
+import query from '../queries/fetchSongs'
 
 class SongCreate extends Component {
   constructor (props) {
@@ -15,13 +18,15 @@ class SongCreate extends Component {
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
-    })
+      },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'))
   }
 
   render () {
     return (
       <div>
+        <Link to='/'>Back</Link>
         <h3>Create a New Song</h3>
         <form action='' onSubmit={this.onSubmit.bind(this)}>
           <label htmlFor=''>Song Title:</label>
