c92f2d2c28649aa347b22d18aed39e3e8d3a78e9
oopserv <oopserv@gmail.com>
2017-05-30 16:17:35 -0500

    Update mutation for lyricLikes

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LyricList extends Component {
-  onLike (id) {
-    console.log(id)
+  onLike (id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typeName: 'Mutation',
+        likeLyric: {
+          id: id,
+          __typeName: 'lyricType',
+          likes: likes + 1
+        }
+      }
+    })
   }
   renderLyrics () {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
-          <i className='material-icons' onClick={() => this.onLike(id)}>thumb_up</i>
+          <div className='vote-box'>
+            <i className='material-icons' onClick={() => this.onLike(id, likes)}>thumb_up</i>
+            {likes}
+          </div>
         </li>
       )
     })
@@ -23,4 +38,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList
+const mutation = gql`
+  mutation Likelyric($id: ID){
+    likeLyric(id: $id){
+      id
+      likes
+    }
+  }
+`
+
+export default graphql(mutation)(LyricList)
